3d303be0459873541ef7a627221e555726c1a163
leonordd <leonorddomingues3@gmail.com>
2024-01-07 00:00:09 +0000

    update css geral

--- js/buildArtifactPage.js
+++ js/buildArtifactPage.js
@@ -188,22 +188,3 @@ function displayArtifact(data) {
         throw error;
     }
 })();
-
-
-
-/*JS GERAL -----------------------------------------------------------------------*/
-/* Reference code: https://codepen.io/whipcat/pen/ExKPQqZ  */
-$("html").mousemove(function (event) {
-    var eye = $(".eye");
-    console.log('eye', eye)
-    var x = (eye.offset().left) + (eye.width() / 2);
-    var y = (eye.offset().top) + (eye.height() / 2);
-    var rad = Math.atan2(event.pageX - x, event.pageY - y);
-    var rot = (rad * (180 / Math.PI) * -1) + 180;
-    eye.css({
-        '-webkit-transform': 'rotate(' + rot + 'deg)',
-        '-moz-transform': 'rotate(' + rot + 'deg)',
-        '-ms-transform': 'rotate(' + rot + 'deg)',
-        'transform': 'rotate(' + rot + 'deg)'
-    });
-});
