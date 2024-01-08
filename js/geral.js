8e2238b89a3d89a910175a404e88b215e75d0b54
leonordd <leonorddomingues3@gmail.com>
2024-01-08 18:20:53 +0000

    update menu bar homepage and add new script eyes

--- js/geral.js
+++ js/geral.js
@@ -1,23 +1,5 @@
 /*JS GERAL -----------------------------------------------------------------------*/
 
-let eyes = document.querySelector("#eyes");
-let fullscreen = document.querySelector(".fullscreen-menu");
-let header = document.querySelector("header");
-let p = document.querySelectorAll("header p");
-
-eyes.addEventListener("click", function () {
-    fullscreen.classList.toggle('show');
-
-    for (let i = 0; i < p.length; i++) {
-        if (p[i].style.opacity === "0") {
-            p[i].style.opacity = "1";
-        } else {
-            p[i].style.opacity = "0";
-        }
-    }
-});
-
-
 // Reference code: https://codepen.io/whipcat/pen/ExKPQqZ 
 $("html").mousemove(function (event) {
     var eye = $(".eye");
