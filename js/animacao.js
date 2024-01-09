476e1ab9eb6a9d79de789b8f23ba16427c762ff1
Hirvikallio <carolinahirvikallio@gmail.com>
2024-01-09 01:25:02 +0000

    testes

--- js/animacao.js
+++ js/animacao.js
@@ -1,21 +1,23 @@
 const animacao = document.querySelector('.animacao');
 const zoomImage = document.querySelector('.zoom-effect');
-const intro = document.querySelector('.intro');
 
 window.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY;
 
     // Adjust these values to control the zoom and transparency effect
-    const zoomFactor = 1 + scrollPosition / 1000;
-    const opacityFactor = 1 - scrollPosition / 500;
+    const zoomFactor = 1 + scrollPosition / 500; // Adjust as needed
+    const opacityFactor = 1 - scrollPosition / 1500; // Adjust as needed
+
+    // Check if the image is not visible
+    if (opacityFactor <= 0) {
+        // Set a lower z-index to move the image behind everything
+        zoomImage.style.zIndex = '-10';
+    } else {
+        // Restore the original z-index when the image is visible
+        zoomImage.style.zIndex = '7';
+    }
 
     // Apply the zoom and transparency effects to the image
     zoomImage.style.transform = `scale(${zoomFactor})`;
     zoomImage.style.opacity = opacityFactor;
-
-    // Only move the intro when the image is more zoomed
-    if (zoomFactor > 2) {
-        const introTranslateY = Math.min(0, scrollPosition - animacao.offsetTop);
-        intro.style.transform = `translateY(${introTranslateY}px)`;
-    }
 });
